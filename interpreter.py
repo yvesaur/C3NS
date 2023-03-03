8b33d2b8b2ad229f442ba992e97c7eab04ad69f6
Yves <yvescasio@yahoo.com>
2023-03-04 03:46:13 +0800

    Basic lexical analyzer

---
+++ interpreter.py
@@ -0,0 +1,11 @@
+import lexer
+
+print("INPUT CODE: ")
+while True:
+    text = input(">")
+    
+    result, error = lexer.run('<stdin>',text)
+
+    if error: print(error.as_string())
+    else: print(result)
+
